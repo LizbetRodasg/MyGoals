26ece04fb6052f93708dcfd0bf56fc6c0906badf
LizbetRodasg <lizbethrodas43@gmail.com>
2024-05-01 14:35:40 -0700

    1 DE MAYO

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react';
+import {Button, StyleSheet, Text,TextInput, View,ScrollView,FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText,setEnteredGoalText]=useState('')
+  conts [goals,setGoals]=useState([])
+
+  function handleInputGoal(enteredText) {
+    console.log(enteredText)
+    setEnteredGoalText(enteredText)
+  }
+   
+  function handleAddGoal() {
+    console.log (enteredGoalText)
+    setGoals(() => [...goals, {Text: enteredGoalText,key: Mat. random().toString()}])
+  }
+  //Como quiero que se vea el componente
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+        style={styles.textInput}
+        placeholder='Your Goal!'
+        onChangeText={handleAddGoal}
+    />
+    <Button
+    title='Add Goal'
+    color={"#A3FFD6"}
+    onPress={handleAddGoal}
+    />
+    </View>
+    <View style ={styles.goalsContainer}>
+    <FlatList
+    data={goals}
+    renderItem= { (itemData) => {
+      <View style ={styles.goalsItem}>
+      <Text style ={styles.goalText}>
+        {itemData.item.text}
+        </Text>
+        </View>
+    }}
+    >
+      </FlatList>
+    </View>
     </View>
   );
 }
-
+//Se comporta como Css
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:50,
+    paddingHorizontal:20
+  },
+  inputContainer:{
+    flex: 1,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+    marginBotton: 20,
+    borderBottonWidth: 1,
+    borderBottonColor: '#7BC9FF'
+  },
+  btnGoal:{
+    borderRadius:20,
+    backgroundColor:"#cccccc"
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'80%',
+    marginRight:3,
+    padding:8,
+    borderRadius:5,
+  },
+  goalsContainer:{
+    flex:5
+  },
+  goalsItem:{
+    margin: 8,
+    padding:8,
+    borderRadius:6,
+    backgroundColor:'#8576FF',
+    color: 'white'
+  },
+  goalText:{
+    color: 'white'
   },
 });
